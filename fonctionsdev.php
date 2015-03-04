e9a5ba2587c6edff45b5a643a32b2319c5d35be4
FindYourSelf <baptiste.meunier0@gmail.com>
2015-03-04 21:32:09 +0100

    Update fonctionsdev.php

--- fonctionsdev.php
+++ fonctionsdev.php
@@ -1,17 +1,16 @@
 <?php
-	function debug($var){
+function debug($var){
 		
-		$debug = debug_backtrace();
-		echo'<a href="#"><strong>'.$debug[0]['file'].'</strong></a> l.'.$debug[0]['line'];
-		echo'<ol>';
-		foreach($debug as $k=>$v){ if($k<0){
-			echo'<li><strong>'.$v['file'].'</strong> l.'.$v['line'].'</li>';
-		}}
-		echo'</ol>';
-
+	$debug = debug_backtrace();
+	echo'<a href="#"><strong>'.$debug[0]['file'].'</strong></a> l.'.$debug[0]['line'];
+	echo'<ol>';
+	foreach($debug as $k=>$v){ if($k<0){
+		echo'<li><strong>'.$v['file'].'</strong> l.'.$v['line'].'</li>';
+	}}
+	echo'</ol>';
 	
-	echo'<pre>';
-	print_r($var);//$var
-	echo'</pre>';
-	}
+echo'<pre>';
+print_r($var);//$var
+echo'</pre>';
+}
 ?>
