2d926ecf8c86f9d0b7ede1e5933a8e20e109f815
yanisr49 <yanisrichard@live.fr>
2015-03-07 18:24:10 +0100

    Create nav.php
    menu(s) du sites

---
+++ nav.php
@@ -0,0 +1,7 @@
+<nav>
+	<h2>Inserer un element</h2>
+	<button>Instruction</button>
+	<button>Si ... Alors ... Sinon ...</button>
+	<button>Tant que ... Faire ...</button>
+	<button>Pour ... Faire ...</button>
+</nav>
