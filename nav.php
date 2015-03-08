620d4b1801620c56eaccb469c9868e52ea8e58e2
yanisr49 <yanisrichard@live.fr>
2015-03-08 14:19:45 +0100

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <nav>
 	<h2 id="titleMenu">Premier element</h2>
 	<button id="condition">Condition</button>
-	<button id="afficher">Afficher</button>
+	<button id="write">Afficher</button>
 	<button id="instruction">Instruction</button>
 	<button id="ifElse">Si ... Alors ... Sinon ...</button>
 	<button id="while">Tant que ... Faire ...</button>
