006bfaba501e301ed435267385af9c2d1f1b8b48
Baptiste Meunier <baptiste.meunier0@gmail.com>
2015-03-09 19:15:37 +0100

    Deplacement Connexion SQL
    Placée dans le fichier Config

--- Class/Export.php
+++ Class/Export.php
@@ -11,33 +11,21 @@ Class Export{
 	function __construct($code_recu, $code_casio){
 		$time = time(); 
 		//$this->savedb($code_recu, $time);
-		$this->savetxt($code_casio, $time);
+		//$this->savetxt($code_casio, $time);
 	}
 
-	function savedb($code){
-		$dsn = 'mysql:dbname=isn;host=localhost';
-		$user = 'root';
-		$password = '';
-		try {
-		    $db = new PDO($dsn, $user, $password);
-		}catch(PDOException $e) {
-		    echo 'Connexion échouée : ' . $e->getMessage();
-		}
-		$req = $db->prepare("INSERT INTO programmes(user_id, time, programme) VALUES(:user_id, :time, :programme)");
-		$req->execute(array(
+	function savedb($code, $time){
+		$sql = Config::sql_connect();
+		$requete = $sql->prepare("INSERT INTO programmes(user_id, time, programme) VALUES(:user_id, :time, :programme)");
+		$requete->execute(array(
 			'user_id' => 1,
 			'time' => $time,
 			'programme' => $code
 		));
 	}
 	function savetxt($code, $time){
-		// 1 : on ouvre le fichier
 		$fichier = fopen("save_code/".$time.'.txt', 'a+');
 		fwrite($fichier, $code);
-		debug($fichier);
-		// 2 : on fera ici nos opérations sur le fichier...
-
-		// 3 : quand on a fini de l'utiliser, on ferme le fichier
 		fclose($fichier);
 	}
 }
