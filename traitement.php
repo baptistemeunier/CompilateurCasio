c5e71876bdc0e069f1b2ea205852cf32585e7f2d
Baptiste Meunier <baptiste.meunier0@gmail.com>
2015-03-09 19:15:48 +0100

    Appel de la fonction Config

--- traitement.php
+++ traitement.php
@@ -2,6 +2,7 @@
 /* Appel des fonction de developement */ 
 require "fonctionsdev.php"; 
 /* Appel des differantes classe */
+require "Class/Config.php";
 require "Class/Decode.php";
 require "Class/Code.php";
 require "Class/Run.php";
@@ -9,10 +10,9 @@ require "Class/Export.php";
 
 //$data = "[AFFICHER Entre A]@[LIRE A]@[AFFICHER Entre B]@[LIRE B]@[CALCUL C]A+B+5[/]@[AFFICHER C]"; // Code reçu par la formulaire
 //$data = "[CALCUL A]5[/]@[AFFICHER A =]@[AFFICHER A]";
-$data       = "[AFFICHER Resolution d'equation dans R]@[AFFICHER Ax^2+Bx+c]@[AFFICHER Saisir A]@[LIRE A]@[AFFICHER Saisir B]@[LIRE B]@[AFFICHER Saisir C]@[LIRE C]@[CALCUL D]B^2-4*A*C[/]@[AFFICHER Delta =]@[AFFICHER D]";
+$data       = "[AFFICHER Resolution d'equation dans R]@[AFFICHER Ax^2+Bx+c]@[AFFICHER Saisir A]@[LIRE A]@[AFFICHER Saisir B]@[LIRE B]@[AFFICHER Saisir C]@[LIRE C]@[CALCUL D]B^2-4*A*C[/]@[AFFICHER Delta =]@[AFFICHER D]@[SI D<0][AFFICHER Aucune solution][/]@";
 $codepropre = new Decode($data);                             // Decode le code recu par le formulaire
 $code       = new Code($codepropre->decode);                       // Formate le code en langage Casio
 debug($code->code);                                          // Affichage du code
-$run        = new Run($codepropre->decode, $codepropre->var_used);  // Test de programme envoyé
-$save       = new Export($data , $code->code);  // Test de programme envoyé
-
+//$run        = new Run($codepropre->decode, $codepropre->var_used);  // Test de programme envoyé
+$save       = new Export($data , $code->code);  // Sauvegarde (bdd et txt)
