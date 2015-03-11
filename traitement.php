b9b8741c762a7bcb9e4af61033611fd2ba11b92c
Baptiste Meunier <baptiste.meunier0@gmail.com>
2015-03-11 10:34:10 +0100

    Test de la Class RUN html+js

--- traitement.php
+++ traitement.php
@@ -1,6 +1,8 @@
+<!DOCTYPE html>
+<html>
 <?php
-/* Appel des fonction de developement */ 
-require "fonctionsdev.php"; 
+/* Appel des fonction de developement */
+require "fonctionsdev.php";
 /* Appel des differantes classe */
 require "Class/Config.php";
 require "Class/Decode.php";
@@ -8,15 +10,35 @@ require "Class/Code.php";
 require "Class/Run.php";
 require "Class/Export.php";
 
-//SI D<0][AFFICHER Aucune solution][/]@
-$data       = "AFFICHER Resolution d'equation dans R#AFFICHER Ax^2+Bx+c#AFFICHER Saisir A#LIRE A#AFFICHER Saisir B#LIRE B#AFFICHER Saisir C#LIRE C#CALCUL D=B^2-4*A*C#CLRTXT#STOP#AFFICHER Delta =#AFFICHER D";
+
+//$data       = "AFFICHER Resolution d'equation dans R#AFFICHER Ax^2+Bx+c#AFFICHER Saisir A#LIRE A#AFFICHER Saisir B#LIRE B#AFFICHER Saisir C#LIRE C#CALCUL D=B^2-4*A*C#CLRTXT#STOP#AFFICHER Delta =#AFFICHER D";
+$data = "SET deg#CLRTXT"; // Data de test
+if(isset($_POST['data'])){ // Si une données est envoyée
+	$data = $_POST['data'];
+}
 $codepropre = new Decode($data); // Decode le code recu par le formulaire
 if(empty($codepropre->erreur)){  // Si aucune erreur
 	$code = new Code($codepropre->decode);   // Formate le code en langage Casio
-	//debug($code->code);  // Affichage du code
-	$run        = new Run($codepropre->decode, $codepropre->var_used);  // Test de pro envoyé
+	debug($code->code);  // Affichage du code
+	//$run        = new Run($codepropre->decode, $codepropre->var_used);  // Test de pro envoyé
 	$save = new Export($data , $code->code); // Sauvegarde (bdd et txt)
 }else{
 	debug($codepropre->erreur); // Affichage des erreurs
-	debug($codepropre->decode);
 }
+?>
+<head>
+	<title></title>
+		<script type="text/javascript" src="jquery-1.11.2.min.js"></script>
+		<script type="text/javascript">
+		<?= $run->js; ?>
+		function afficher(){
+			this.append("test");
+		}
+		</script>
+</head>
+<body>
+	<div id="console">
+		<p></p>
+	</div>
+</body>
+</html>
