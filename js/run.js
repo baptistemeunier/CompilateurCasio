be1fe183ae0984db9dba4c2a47782085582a4dd4
FindYourSelf <baptiste.meunier0@gmail.com>
2015-03-13 19:45:03 +0100

    MAJ de la console Run

--- js/run.js
+++ js/run.js
@@ -1,31 +1,32 @@
-
 $(init);
+var n = 0;
+var input;
 function init(){
 	run_instruction();
-	//console.log(liste_instruction);
-	//console.log(instruction);
 	$('#next').on('click', function(){
 		run_instruction();
+		return false;
 	});
 	$('#send').on('click', function(){
 		var value = $('#input').val();
 		if($.isNumeric(value)){
+			$("#text-console").append(value + "<br />");
 			window[input] = parseFloat(value);
-			console.log(window[input]);
 			run_instruction();
 		}
 		return false;
 	});
 }
+
 function run_instruction(){
-	fn = window[liste_instruction[instruction]['fonction']];
-	fn(liste_instruction[instruction]['params']);
-	instruction++;
+	console.log(liste_instruction[n]['fonction']);
+	fn = window[liste_instruction[n]['fonction']];
+	fn(liste_instruction[n]['params']);	
+	n++;
 }
+
 function afficher(params){
-	$("#input").hide();
-	$("#send").hide();
-	$("#next").show();
+	set();
 	if(typeof params['text'] != 'undefined'){
 		$("#text-console").append(params['text'] + "<br />");
 		return false;
@@ -35,9 +36,35 @@ function afficher(params){
 		return false;
 	}		
 }
+
+function instruction(params){
+	set();
+	switch(params){
+		case "Clrtxt":
+			$("#text-console").empty();
+		break;
+		case "Stop":
+			$("#next").hide();
+			$("#input").hide();
+			$("#send").hide();
+		break;
+	}
+}
+
 function lire(params){
-	$("#next").hide();
-	$("#input").show();
-	$("#send").show();		
+	set(true);
 	input = params['var'];
+	$("#text-console").append("> ");
+}
+
+function set(write){
+	if(write){
+		$("#next").hide();
+		$("#input").show();
+		$("#send").show();
+	}else{
+		$("#input").hide();
+		$("#send").hide();
+		$("#next").show();
+	}
 }
--- jsonrun.php
+++ jsonrun.php
@@ -1,12 +1,10 @@
 <?php
 /* Appel des differantes classe */
-$data = "AFFICHER Resolution d'equation dans R#AFFICHER Ax^2+Bx+c#AFFICHER Saisir A#LIRE A#AFFICHER A#AFFICHER Saisir B#LIRE B#AFFICHER Saisir C#LIRE C#CALCUL D=B^2-4*A*C#CLRTXT#STOP#AFFICHER Delta =#AFFICHER D";
+$data = "AFFICHER Resolution d'equation dans R#AFFICHER Ax^2+Bx+c#AFFICHER Saisir A#LIRE A#AFFICHER A#AFFICHER Saisir B#STOP#LIRE B#AFFICHER Saisir C#LIRE C#CALCUL D=B^2-4*A*C#CLRTXT#STOP#AFFICHER Delta =#AFFICHER D";
 require "Class/Config.php";
 require "Class/Decode.php";
 $codepropre = new Decode($data); // Decode le code recu par le formulaire
 ?>
 <script type="text/javascript">
-	var instruction = 0;
-	var input = "";
 	<?= "var liste_instruction = ".json_encode($codepropre->decode).";"; ?>
 </script>
