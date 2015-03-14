f1cbee1e37aef65c1bd28b3d92ad5d41696b35ca
yanisr49 <yanisrichard@live.fr>
2015-03-14 15:31:19 +0100

    improve
    UTF-8 / nav fonction operationnel / fonction SET, CLRTXT ajouté

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html lang="EN" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="FR" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta http-equiv="content-type" content="text/xml; charset=iso-8859-1" />
+		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="index.css" />
 		<link rel="stylesheet" type="text/css" href="nav.css" />
-		<script type="text/javascript" src="jquery-1.11.2.min.js"></script>
-		<script type="text/javascript" src="index.js"></script>
+		<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+		<script type="text/javascript" src="js/index.js"></script>
 		<title>Casio</title>
 	</head>
 	<body id="body">
