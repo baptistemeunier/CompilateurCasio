75c9070f719fa4cfe91628beb65c4148ca2acaf6
FindYourSelf <baptiste.meunier0@gmail.com>
2015-03-15 20:24:01 +0100

    Ajouts d'un interface lors de l'envoi d'un code

--- traitement.php
+++ traitement.php
@@ -8,8 +8,6 @@ require "Class/Code.php";
 require "Class/Run.php";
 require "Class/Export.php";
 
-
-$data = "AFFICHER Test calcul C=A*B#AFFICHER Saisir A#LIRE A#AFFICHER Saisir B#LIRE B#CALCUL C=A*B#AFFICHER C =#AFFICHER C";
 $data = "AFFICHER C=A/B#AFFICHER Saisir A#LIRE A#AFFICHER Saisir B#LIRE B#CALCUL C=A/B#AFFICHER C =#AFFICHER C";
 if(isset($_POST['data'])){ // Si une données est envoyée
 	$data = $_POST['data'];
@@ -17,10 +15,54 @@ if(isset($_POST['data'])){ // Si une données est envoyée
 $codepropre = new Decode($data); // Decode le code recu par le formulaire
 if(empty($codepropre->erreur)){  // Si aucune erreur
 	$code = new Code($codepropre->decode);   // Formate le code en langage Casio
-	debug($code->code);  // Affichage du code
-	//$run        = new Run($codepropre->decode, $codepropre->var_used);  // Test de pro envoyé
 	$save = new Export($data , $code->code); // Sauvegarde (bdd et txt)
 }else{
 	debug($codepropre->erreur); // Affichage des erreurs
 }
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Affichage du programme</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/traitement.css">
+	</head>
+	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript">
+		<?= "var liste_instruction = ".json_encode($codepropre->decode).";"; ?>
+	</script>
+
+	<script type="text/javascript" src="js/run.js"></script>
+	<body>
+		<section id="console">
+			<h2>Testez votre programme !</h2>
+			<div class="console">
+				<p id="text-console"></p>
+			</div>
+			<div class="input">
+				<form>
+					<input type="number" id="input">
+					<button id="send">Send</button>
+				</form>
+				<button id="next">Next</button>
+			</div>
+		</section>
+		<section id="code">
+			<h2>Votre programme au format CASIO</h2>
+			<?= "<pre>".$code->code."</pre>" // Affichage du code ?>
+		</section>
+		<!-- Section Saugegarde (BDD et Text )-->
+		<section id="save">
+			<h2>Sauvegarde de votre programme</h2>
+			<?php 
+			if($save->saved_db != false){
+				echo'<p class="valid">-> Programme sauvegardé dans votre profil</p>';
+			}else{ 
+				echo'<p class="error">-> Programme non sauvegardé dans votre profil</p>';
+			}
+			if($save->saved_txt != false){
+				echo'<p class="valid">-> Programme exporté au format .txt <a href="save_code/'.$save->saved_txt.'.txt" target="_blank">Télécharger</p>';
+			} ?>
+		</section>
+	</body>
+</html>
