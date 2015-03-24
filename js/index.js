c13df3600a9668b8a2a00fcb5affe53a5a061d78
FindYourSelf <baptiste.meunier0@gmail.com>
2015-03-24 19:46:43 +0100

    PULL resquest Yanis merge via console

--- js/index.js
+++ js/index.js
@@ -1,38 +1,67 @@
 $(init);
 
-/* Fonction d'initialisation */
+/* Fonction qui initilise le programmr */
 function init(){
 
-	// L'instruction condition n'est possible que sur les boucles donc pas sur un programme vierge
+	/* Initialisation de la page */
 	$('#condition').hide();
+	elementSelect = $('.plus').get(0);
 
-	// Initialisation des variables globales
-	var elementSelect = $('.plus').get(0);
+	/* Initialisation des fonctions */
+	ifThenElse();
+	whileDo();
+	forDo();
+	instructionShow();
+	calcul();
+	read();
+	condition();
+	set();
+	clearTxt();
 
-	/* Affichage de la structure If Else sur un passage de souris (hover) */
+
+	$('.plus').on('click', function(){
+		if(this.parentNode.className == 'if' || this.parentNode.className == 'while'){
+			hideButtons();
+			$('#condition').show();
+		}else{
+			hideButtons();
+			showButtons();
+		}
+		return false;
+	});
+
+	/* On enlève le menu lorsqu'un élément a été sélectionné */
+	$('#navLeft button').on('click', function(){
+		hideButtons();
+	});
+}
+
+
+/* Fonction global qui affiche la structure IfThenElse */
+function ifThenElse(){
 	$('#ifElse').on('mouseenter', function(){
-			$(elementSelect).before('<button class="plus">+</button>');
-			$(elementSelect).before('<div class="if"></div>');
-			$(elementSelect.previousSibling).append('<h3>Si</h3>');
-			$(elementSelect.previousSibling).append('<input type="text" name="#" class="si" placeholder="Si ...">');
-			$(elementSelect.previousSibling).append('<button class="plus">+</button>');
-			$(elementSelect).before('<div class="then"></div>');
-			$(elementSelect.previousSibling).append('<h3>Alors</h3>');
-			$(elementSelect.previousSibling).append('<button class="plus">+</button>');
-			$(elementSelect).before('<div class="else"></div>');
-			$(elementSelect.previousSibling).append('<h3>Sinon</h3>');
-			$(elementSelect.previousSibling).append('<button class="plus">+</button>');
-			$(elementSelect.previousSibling).addClass('mouseOn');
-			$(elementSelect.previousSibling.previousSibling).addClass('mouseOn');
-			$(elementSelect.previousSibling.previousSibling.previousSibling).addClass('mouseOn');
+		$(elementSelect).before('<button class="plus">+</button>');
+		$(elementSelect).before('<div class="if"></div>');
+		$(elementSelect.previousSibling).append('<h3>Si</h3>');
+		$(elementSelect.previousSibling).append('<input type="text" name="condition" placeholder="Si ...">');
+		$(elementSelect.previousSibling).append('<button class="plus">+</button>');
+		$(elementSelect).before('<div class="then"></div>');
+		$(elementSelect.previousSibling).append('<h3>Alors</h3>');
+		$(elementSelect.previousSibling).append('<button class="plus">+</button>');
+		$(elementSelect).before('<div class="else"></div>');
+		$(elementSelect.previousSibling).append('<h3>Sinon</h3>');
+		$(elementSelect.previousSibling).append('<button class="plus">+</button>');
+		$(elementSelect.previousSibling).addClass('mouseOn');
+		$(elementSelect.previousSibling.previousSibling).addClass('mouseOn');
+		$(elementSelect.previousSibling.previousSibling.previousSibling).addClass('mouseOn');
 	});
 	/* Affichage de la structure If Else Then sur un click */
 	$('#ifElse').on('click', function(){
-			$(elementSelect.previousSibling).removeClass('mouseOn');
-			$(elementSelect.previousSibling.previousSibling).removeClass('mouseOn');
-			$(elementSelect.previousSibling.previousSibling.previousSibling).removeClass('mouseOn');
-			elementSelect = "cliquer";
-			conditionSi();
+		$(elementSelect.previousSibling).removeClass('mouseOn');
+		$(elementSelect.previousSibling.previousSibling).removeClass('mouseOn');
+		$(elementSelect.previousSibling.previousSibling.previousSibling).removeClass('mouseOn');
+		elementSelect = "cliquer";
+		conditionSi();
 	});
 	/* Efface la structure If Else si le bouton n'a pas été activé */
 	$('#ifElse').on('mouseleave', function(){
@@ -42,18 +71,17 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
+}
 
-
-
-
-
-	/* Affichage de la boucle While sur un passage de souris (hover) */
+/* Fonction global qui affiche la strucutre while */
+function whileDo(){
 	$('#while').on('mouseenter', function(){
 		$(elementSelect).before('<button class="plus">+</button>');
 		$(elementSelect).before('<div class="while"></div>');
 		$(elementSelect.previousSibling).append('<h3>Tant que</h3>');
-		$(elementSelect.previousSibling).append('<input type="text" name="#" class="si" placeholder="Tant que">');
+		$(elementSelect.previousSibling).append('<input type="text" name="condition" placeholder="Tant que">');
 		$(elementSelect.previousSibling).append('<button class="plus">+</button>');
 		$(elementSelect).before('<div class="do"></div>');
 		$(elementSelect.previousSibling).append('<h3>Faire</h3>');
@@ -75,13 +103,11 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
+}
 
-
-
-
-
-	/* Affichage de la boucle For sur un passage de souris (hover) */
+function forDo(){
 	$('#for').on('mouseenter', function(){
 		$(elementSelect).before('<button class="plus">+</button>');
 		$(elementSelect).before('<div class="for"></div>');
@@ -108,12 +134,11 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
+}
 
-
-
-
-
+function instructionShow(){
 	/* Afficher l'instruction Affichage sur un passage de la souris */
 	$('#afficher').on('mouseenter', function(){
 		$(elementSelect).before('<button class="plus">+</button>');
@@ -132,12 +157,11 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
+}
 
-
-
-
-
+function calcul(){
 	/* Afficher l'instruction Calcul sur un passage de la souris */
 	$('#calcul').on('mouseenter', function(){
 		$(elementSelect).before('<button class="plus">+</button>');
@@ -156,10 +180,11 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
+}
 
-
-
+function read(){
 	var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 	/* Afficher l'instruction Lire sur un passage de la souris */
@@ -184,12 +209,11 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
+}
 
-
-
-
-
+function condition(){
 	/* Afficher l'instruction Condition sur un passage de la souris */
 	$('#condition').on('mouseenter', function(){
 		$(elementSelect).before('<button class="plus">+</button>');
@@ -200,6 +224,7 @@ function init(){
 	$('#condition').on('click', function(){
 		$(elementSelect.previousSibling).removeClass('mouseOn');
 		elementSelect = "cliquer";
+		conditionSi();
 		formatageProgramme();
 	});
 	/* Efface l'instruction Condition si le bouton n'a pas été activé */
@@ -208,12 +233,11 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
+}
 
-
-
-
-
+function clearTxt(){
 	/* Afficher la fonction ClearText sur un passage de la souris */
 	$('#clearText').on('mouseenter', function(){
 		$(elementSelect).before('<button class="plus">+</button>');
@@ -234,12 +258,11 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
+}
 
-
-
-
-
+function set(){
 	/* Afficher la fonction Set sur un passage de la souris */
 	$('#set').on('mouseenter', function(){
 		$(elementSelect).before('<button class="plus">+</button>');
@@ -259,59 +282,16 @@ function init(){
 			$(elementSelect.previousSibling).remove();
 			$(elementSelect.previousSibling).remove();
 		}
+		getElement();
 	});
-	
-
-	
-	
-
-
-	
-	/* On modifie le menu en fonction de l'élément sélectionné */
-	$('#navLeft button').on('click', function(){
-		
-		/* On affiche l'ecran de selection */
-		$('#titleMenu').html('Veuillez saisir un element !!!');
-		hideButtons();
-
-		/* Evenement du bouton "plus" */
-		$('.plus').on('click', function(){
-			if(this.parentNode.className == 'if' || this.parentNode.className == 'while'){
-				$('#titleMenu').html('Ajouter une condition');
-				hideButtons();
-				$('#condition').show();
-			}else if(this.parentNode.className == 'then'){
-				$('#titleMenu').html('Modifier le ALORS');
-				hideButtons();
-				showButtons()
-			}else if(this.parentNode.className == 'else'){
-				$('#titleMenu').html('Modifier le SINON');
-				hideButtons();
-				showButtons()
-			}else if(this.parentNode.className == 'do'){
-				$('#titleMenu').html('Modifier le FAIRE');
-				hideButtons();
-				showButtons()
-			}else if(this.parentNode.id == 'programme'){
-				$('#titleMenu').html('Ajouter au programme');
-				hideButtons();
-				showButtons()
-			}
-			elementSelect = this;
-			return false;
-		});
-	});
-
-	$('#submit').on('click', function(){
-		$('form').submit();
-	})
-
-	
 }
 
 /* Fonction pour masquer tout les buttons */
 function hideButtons(){
 	$('#clearText').hide();
+	$('#label').hide();
+	$('#goTo').hide();
+	$('#draw').hide();
 	$('#set').hide();
 	$('#afficher').hide();
 	$('#calcul').hide();
@@ -325,6 +305,9 @@ function hideButtons(){
 /* Fonction pour afficher les buttons récurents */
 function showButtons(){
 	$('#clearText').show();
+	$('#label').show();
+	$('#goTo').show();
+	$('#draw').show();
 	$('#set').show();
 	$('#afficher').show();
 	$('#calcul').show();
@@ -334,10 +317,34 @@ function showButtons(){
 	$('#for').show();
 }
 
+/* Evenement du bouton "plus" */
+function getElement(){
+	$('.plus').on('click', function(){
+		if(this.parentNode.className == 'if' || this.parentNode.className == 'while'){
+			hideButtons();
+			$('#condition').show();
+		}else{
+			hideButtons();
+			showButtons();
+		}
+		elementSelect = this;
+		return false;
+	});
+}
+
+function validateForm(){
+	if(document.forms["form"]["data"].value == '' || document.forms["form"]["data"].value == NULL){
+		alert("Votre programme est vide !!!");
+		return false;
+	}else {
+		return true;
+	}
+}
+
 function formatageProgramme(){
 	$('input').on('keyup', function(){
 		var test = "";
-		var valeurForm = document.getElementById('programme');
+		var valeurForm = document.getElementById('formulaire');
 		for(var i=0; i<valeurForm.length; i++)
 		{
 			if(valeurForm.elements[i].className != "plus" && valeurForm.elements[i].id != "data" && valeurForm.elements[i].id != "title" && valeurForm.elements[i].className != "checkbox")
@@ -345,7 +352,7 @@ function formatageProgramme(){
 				test += valeurForm.elements[i].className.toUpperCase()
 				if(valeurForm.elements[i].value != "CLRTXT") test += ' ';
 				test += valeurForm.elements[i].value;
-				if(i < valeurForm.length-8)
+				if(i < valeurForm.length-7)
 				{
 					test += '#';
 				}
@@ -356,13 +363,13 @@ function formatageProgramme(){
 }
 
 function conditionSi(){
-	$('.si').on('focus', function(){
+	$('input[name=condition]').on('focus', function(){
 		var valeur = this.value;
 		if(!valeur.match(/^ *([a-zA-Z]+|[0-9]+) *(<|>|==|<=|>=|!=) *([a-zA-Z]+|[0-9]+) *$/) || !valeur.match(/^ *(".+"|[a-zA-Z]+) *(==|!=) *(".+"|[a-zA-Z]+) *$/)){
 			this.style.boxShadow = '0 0 30px red';
 		}
 	});
-	$('.si').on('keyup', function(){
+	$('input[name=condition]').on('keyup', function(){
 		var valeur = this.value;
 		if(valeur.match(/^ *([a-zA-Z]+|[0-9]+) *(<|>|==|<=|>=|!=) *([a-zA-Z]+|[0-9]+) *$/) || valeur.match(/^ *(".+"|[a-zA-Z]+) *(==|!=) *(".+"|[a-zA-Z]+) *$/)){
 			this.style.boxShadow = '0 0 15px lime';
@@ -370,7 +377,7 @@ function conditionSi(){
 			this.style.boxShadow = '0 0 30px red';
 		}
 	});
-	$('.si').on('blur', function(){
+	$('input[name=condition]').on('blur', function(){
 		var valeur = this.value;
 		if(valeur.match(/^ *([a-zA-Z]+|[0-9]+) *(<|>|==|<=|>=|!=) *([a-zA-Z]+|[0-9]+) *$/) || valeur.match(/^ *(".+"|[a-zA-Z]+) *(==|!=) *(".+"|[a-zA-Z]+) *$/)){
 			this.style.boxShadow = '0 0 0 white';
--- nav.php
+++ nav.php
@@ -1,7 +1,10 @@
 <nav id="navLeft">
-	<h2 id="titleMenu">Premier élément</h2>
+	<h2 id="titleMenu">Eléments</h2>
 	<button id="condition">Condition</button>
 	<button id="clearText">ClearText</button>
+	<button id="label">label</button>
+	<button id="goTo">Go To</button>
+	<button id="draw">draw</button>
 	<button id="set">Set (degrés/radian)</button>
 	<button id="afficher">Afficher</button>
 	<button id="calcul">Calculer</button>
@@ -11,5 +14,5 @@
 	<button id="for">Pour ... Faire ...</button>
 </nav>
 <nav id="navRight">
-	<h2>Help</h2>
+	<h2>Aide</h2>
 </nav>
