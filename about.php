48bcaec61cd301514e5e72a4fd7c631ea37975bc
FindYourSelf <baptiste.meunier0@gmail.com>
2015-05-17 15:08:56 +0200

    BASTIEN : Ajout de la page d'accueil

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/menu.css">
+		<title>Casio</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<?php require("menu.php"); ?>
+		<div id="b"> Bienvenue sur notre site internet "Compilateur Casio"</div>
+		<div id="c">Ce site internet a été crée par des terminales pour le projet d'ISN (Informatique et Science du Numérique).
+		Il permet de traduire un langage Basic (métalangage) en langage Casio (utilisé pour les calculatrice)<br/><img src="img/algorithme.png" /><img src="img/fleche.png" width="200px" /> <img src="img/calculatrice.jpg" /> <br/> et de pouvoir exécuter le programme crée. Il sera aussi possible de le sauvegarder votre programme dans la base de données si vous vous inscrivez et decontinuer votre conversion.
+		
+</div>
+	</body>
+</html>
--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <ul id="nav">
-	<li><a href="menu.html">Accueil</a></li>
+	<li><a href="about.php">Accueil</a></li>
 	<li><a href="./">Compilateur</a></li>
 	<?php if(isset($_SESSION['pseudo'])){
 		echo'<li><a href="programmes.php">Mes programmes</a></li>
-		<li><a href="#">Deconnexion</a></li>';
+		<li><a href="deconnexion.php">Deconnexion</a></li>';
 	}else{ ?>
 	<li><a href="inscription.php">Inscription</a></li>
 	<li><a href="connexion.php">Connexion</a></li>
