e5add666649daeece1307a2080b9d62ad9499bb9
FindYourSelf <baptiste.meunier0@gmail.com>
2015-05-18 10:36:38 +0200

    YANIS  Ajout delete

--- nav.php
+++ nav.php
@@ -4,7 +4,6 @@
 	<button id="clearText">ClearText</button>
 	<button id="label">label</button>
 	<button id="goTo">Go To</button>
-	<button id="draw">draw</button>
 	<button id="set">Set (degrés/radian)</button>
 	<button id="afficher">Afficher</button>
 	<button id="calcul">Calculer</button>
@@ -15,4 +14,49 @@
 </nav>
 <nav id="navRight">
 	<h2>Aide</h2>
+	<div id="aideCondition">
+		Rajoute une condition. Si la condition est remplie 
+		alors le programme entrera dans la fontion.
+	</div>
+	<div id="aideClearText">
+		Une fois que le programme lira cette instruction, 
+		il effacera tout l'écran de la calculette.
+	</div>
+	<div id="aideLabel">
+		Fonctionne avec l'élément "Go To", lorsque le programme 
+		lit l'élément "Go To", il va directement au label 
+		correspondant.
+	</div>
+	<div id="aideGoTo">
+		Fonctionne avec l'élément "Label", lorsque le programme 
+		lit cette instruction, il va directement au "label" 
+		correspondant.
+	</div>
+	<div id="aideSet">
+		Permet de régler la calculette pour faire des calculs en 
+		radian ou en degrée.
+	</div>
+	<div id="aideAfficher">
+		Permet d'afficher le texte voulu.
+	</div>
+	<div id="aideCalcul">
+		Permet d'associer à une variable le calcul de variable(s)
+		souhaitée(s).
+	</div>
+	<div id="aideLire">
+		Permet de lire la variable souhaitée à l'utilisateur du 
+		programme.
+	</div>
+	<div id="aideIf">
+		Permet d'éxécuter les instructions voulu à certaines 
+		condiction données.
+	</div>
+	<div id="aideWhile">
+		Permet d'éxécuter les instructions voulu un nombre de 
+		fois voulu donnée sous forme de condition.
+	</div>
+	<div id="aideFor">
+		Peremet d'éxécuter les instructions voulu un nombre de 
+		fois voulu en indiquant un nombre précis de fois.
+	</div>
 </nav>
